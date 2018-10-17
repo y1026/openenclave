7ad379693ee97ab9b26969a815fd4658977b0af6
Akash Gupta (EOSG) <akagup@microsoft.com>
2018-10-17 16:36:44 -0700

    Windows signing works.

--- host/crypto/rsa.h
+++ host/crypto/rsa.h
@@ -5,9 +5,17 @@
 #define _OE_HOST_CRYPTO_RSA_H
 
 #include <openenclave/internal/rsa.h>
-#include <openssl/evp.h>
 
+#if !defined(_WIN32)
+#include <openssl/evp.h>
+#endif
+
+#if !defined(_WIN32)
 /* Caller is responsible for validating parameters */
 void oe_rsa_public_key_init(oe_rsa_public_key_t* public_key, EVP_PKEY* pkey);
+#endif
+
+oe_result_t oe_rsa_get_public_key_from_private(
+    const oe_rsa_private_key_t* private_key, oe_rsa_public_key_t* public_key);
 
 #endif /* _OE_HOST_CRYPTO_RSA_H */
